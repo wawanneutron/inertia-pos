7244c73c7fe031d2bf83f811a2b9a8a045405adc
wawanneutron <wawanneutron1331@gmail.com>
2023-09-06 11:10:02 +0700

    Accessor model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -15,5 +16,11 @@ class Product extends Model
 
    public function category() {
       return $this->belongsTo(Category::class);
+   }
+
+   protected function image(): Attribute {
+    return Attribute::make(
+      get: fn ($value) => asset('/storage/products/' . $value)
+    );
    }
 }
